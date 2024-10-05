5a185fc3538696a08de316bb7749d8d780ba043a
prathmesh khandare <81563330+prathameshkhandare@users.noreply.github.com>
2024-10-05 19:33:02 +0530

    Create LinkedList.java

---
+++ DSA/LinkedList.java
@@ -0,0 +1,71 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    Node head;
+
+    void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    void display() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    void delete(int key) {
+        if (head == null) return;
+
+        if (head.data == key) {
+            head = head.next;
+            return;
+        }
+
+        Node current = head;
+        Node previous = null;
+
+        while (current != null && current.data != key) {
+            previous = current;
+            current = current.next;
+        }
+
+        if (current != null) {
+            previous.next = current.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        
+        System.out.println("Linked List:");
+        list.display();
+
+        list.delete(20);
+        System.out.println("After deleting 20:");
+        list.display();
+    }
+}
